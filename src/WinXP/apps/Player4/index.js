9a55871c41a7d6bf9dd30abc2489d5a4085f9a13
polofesse <paul.jousselin@gmail.com>
2024-10-29 22:43:59 +0100

    cest bon enfin?

--- src/WinXP/apps/Player4/index.js
+++ src/WinXP/apps/Player4/index.js
@@ -120,13 +120,17 @@ const Beretta = () => {
     ) {
       setCaptchaPassed(true);
       setErrorMessage(''); // Réinitialiser le message d'erreur en cas de succès
-
-      // Appel direct à la fonction de changement d'étape après validation
-      handleNextStep();
     } else {
       setErrorMessage('Réponse incorrecte. Essayez encore !');
     }
   };
+  useEffect(() => {
+    if (captchaPassed) {
+      handleNextStep();
+      setCaptchaPassed(false); // Réinitialiser l'état pour éviter des appels répétés
+    }
+  }, [captchaPassed, handleNextStep]);
+
   // useEffect(() => {
   //   if (captchaPassed) {
   //     handleNextStep();
